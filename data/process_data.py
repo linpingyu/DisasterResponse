cb7a5d69f4458f70bff36f54b70b105b7afc123c
Linping Yu <linping.yu@outlook.com>
2018-12-10 00:47:59 -0800

    add if_exists = 'replace' to save_data()

--- data/process_data.py
+++ data/process_data.py
@@ -44,7 +44,7 @@ def save_data(df, database_filename):
     Saves cleaned dataframe into sqlite database
     '''
     engine = create_engine('sqlite:///'+ database_filename)
-    df.to_sql('disasterCleaned', engine, index=False)
+    df.to_sql('disasterCleaned', engine, index=False, if_exists='replace')
 
 def main():
     if len(sys.argv) == 4:
