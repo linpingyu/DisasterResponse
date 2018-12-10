9c64f7471876033c1cadca9826048ffc883334e7
Linping Yu <linping.yu@outlook.com>
2018-12-10 05:16:01 -0800

    re-commit fix indentation error

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -24,29 +24,29 @@ nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger', 'stopwords'])
 
 
 def load_data(database_filepath):
-	'''
-	Loads data from database
-	Returns:
-		X: feature
-		Y: target labels
-		category_names: target categories
-	'''
-	engine = create_engine('sqlite:///' + database_filepath)
-	df = pd.read_sql_table('disasterCleaned', con=engine)
-	X = df.message
-	Y = df.drop(['id', 'message', 'original', 'genre'], axis=1)
-	category_names = Y.columns
-	return X, Y, category_names
+    '''
+    Loads data from database
+    Returns:
+        X: feature
+        Y: target labels
+        category_names: target categories
+    '''
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('disasterCleaned', con=engine)
+    X = df.message
+    Y = df.drop(['id', 'message', 'original', 'genre'], axis=1)
+    category_names = Y.columns
+    return X, Y, category_names
 
 
 def tokenize(text):
-	
-	'''
-	Tokenizes the text after removing punctuation, and stopwords
-	Returns: tokens from the text
-	'''
 
-	#init lemmatizer
+    '''
+    Tokenizes the text after removing punctuation, and stopwords
+    Returns: tokens from the text
+    '''
+
+    #init lemmatizer
     lemmatizer = WordNetLemmatizer()
     stop_words = stopwords.words('english')
     # normalize case and remove punctuation
@@ -57,82 +57,81 @@ def tokenize(text):
     tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
     return tokens
 
-class StartingVerbExtractor(BaseEstimator, TransformerMixin):
-	'''
-	Extracts if the first tag is a verb
-	'''
+# class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+#     '''
+#     Extracts if the first tag is a verb
+#     '''
 
-    def starting_verb(self, text):
-        sentence_list = nltk.sent_tokenize(text)
-        for sentence in sentence_list:
-            pos_tags = nltk.pos_tag(tokenize(sentence))
-            try:
-                first_word, first_tag = pos_tags[0]
-                if first_tag in ['VB', 'VBP'] or first_word == 'RT':
-                    return True
-            except:
-                continue
-        return False
+#     def starting_verb(self, text):
+#         sentence_list = nltk.sent_tokenize(text)
+#         for sentence in sentence_list:
+#             pos_tags = nltk.pos_tag(tokenize(sentence))
+#             try:
+#                 first_word, first_tag = pos_tags[0]
+#                 if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+#                     return True
+#             except:
+#                 continue
+#         return False
 
-    def fit(self, x, y=None):
-        return self
+#     def fit(self, x, y=None):
+#         return self
 
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.starting_verb)
-        return pd.DataFrame(X_tagged)
+#     def transform(self, X):
+#         X_tagged = pd.Series(X).apply(self.starting_verb)
+#         return pd.DataFrame(X_tagged)
     
 
 
 def build_model():
-	'''
-	Builds classification model pipeline using randomforest classifier
-	'''
+    '''
+    Builds classification model pipeline using randomforest classifier
+    '''
     pipeline = Pipeline([
-    	('features', 
-    		FeatureUnion([
-    			('text_pipeline', Pipeline([
-    				('vect', CountVectorizer(tokenizer = tokenize)),
-    				('tfidf', TfidfTransformer())
-    				])
-    			),
-    			('starting_verb', StartingVerbExtractor())
-    		])
-    	),
-    	('clf', MultiOutputClassifier(estimator=RandomForestClassifier()))
+        ('features', 
+         FeatureUnion([
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer = tokenize)),
+                ('tfidf', TfidfTransformer())
+            ])
+            )
+         ])
+        ),
+        ('clf', MultiOutputClassifier(estimator=RandomForestClassifier()))
     ])
-	
+
     #grid search parameters
     parameters = {
-        'features__text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
-        'features__text_pipeline__vect__max_df': (0.5, 0.75, 1.0),
-        'features__text_pipeline__vect__max_features': (None, 5000, 10000),
-        'features__text_pipeline__tfidf__use_idf': (True, False)
+        # 'features__text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
+        # 'features__text_pipeline__vect__max_df': (0.5, 0.75, 1.0),
+        # 'features__text_pipeline__vect__max_features': (None, 5000, 10000),
+        # 'features__text_pipeline__tfidf__use_idf': (True, False)
     }
-	
+
     #gridsearch crossvalidations
-    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=2, n_jobs=4)
+    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=2, n_jobs=4, cv=2)
     return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-	'''
-	Evaluates the model on the test dataset
-	'''
+    '''
+    Evaluates the model on the test dataset
+    '''
 
     y_preds = model.predict(X_test)
     for i, column in enumerate(category_names):
-    	y_pred = [x[i] for x in y_preds]
-    	print(f'For category: {column}')
-    	print(classification_report(y_true = Y_test[column], y_pred = y_pred))
+        y_pred = [x[i] for x in y_preds]
+        print(f'For category: {column}')
+        print(classification_report(y_true = Y_test[column], y_pred = y_pred))
 
 
 
 def save_model(model, model_filepath):
-	'''
-	Saves the model into a Python pickle
-	'''
+    '''
+    Saves the model into a Python pickle
+    '''
 
-	pickle.dump(model, open(model_filepath, 'wb'))
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
